2be45dc4aa795c8041d72c5c4df664efbcdadc16
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2023-10-27 11:13:16 +0530

    build: sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   if (isLoading) {
@@ -16,6 +17,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }
   return (
   <div className=" h-full flex dark:bg-[#1F1F1F]">
+    <Navigation/>
     <main className=" flex-1 h-full overflow-y-auto">
     {children}
     </main>
