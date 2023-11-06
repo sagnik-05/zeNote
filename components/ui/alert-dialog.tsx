6cae9227282ac1dea0c029c3382c670ef2e6ddc1
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2023-11-06 15:14:32 +0530

    ui changes

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
-
+import * as React from "react"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
