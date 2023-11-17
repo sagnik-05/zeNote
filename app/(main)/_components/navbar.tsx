21a389d5bc90d1660296d8e8af5a21d85378340c
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2023-11-17 08:39:00 +0530

    title page navbar created

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -19,7 +19,11 @@ const Navbar = ({
     });
 
     if(document == undefined){
-        return <p>Loading...</p>
+        return (
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+                <Title.Skeleton/>
+            </nav>
+        )
     }
     if(document === null){
         return null;
